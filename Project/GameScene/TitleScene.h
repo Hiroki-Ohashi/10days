fba2b1d0cda0ef5653aa00ba32fe2440591e9fbf
洋輝 大橋 <hiro0084169@outlook.jp>
2024-09-06 10:13:51 +0900

    master

--- Project/GameScene/TitleScene.h
+++ Project/GameScene/TitleScene.h
@@ -41,26 +41,6 @@ private:
 	Camera camera_;
 	TextureManager* textureManager_ = TextureManager::GetInstance();
 
-	std::unique_ptr<Sprite> title_ = nullptr;
-	std::unique_ptr<Sprite> startLog_ = nullptr;
-
 	std::unique_ptr<PostProcess> postProcess_ = nullptr;
-
-	std::unique_ptr<Json> json_ = nullptr;
-	LevelData* levelData_ = nullptr;
-
-	// 天球
-	std::unique_ptr<Skydome> skydome_;
 private:
-	EulerTransform transform;
-
-	float cameraSpeedX = 0.0001f;
-	float cameraSpeedY = 0.0002f;
-
-	uint32_t title;
-	uint32_t start;
-	uint32_t white;
-
-	uint32_t timer;
-	bool blinking;
 };
